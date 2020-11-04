1999c0e4b2b5c51dd737a0c51b57757a56f6afba
Neil Mayo <Neil.Mayo.c10317@mcast.edu.mt>
2020-11-04 18:09:10 +0100

    Created IProductsService, ProductsService, and made the connection between Application Layer and Data Layer

--- SWD62AEP/ShoppingCart.Domain/Models/Product.cs
+++ SWD62AEP/ShoppingCart.Domain/Models/Product.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Dynamic;
 using System.Text;
 
-//code first.
+//code first
 namespace ShoppingCart.Domain.Models
 {
     public class Product
