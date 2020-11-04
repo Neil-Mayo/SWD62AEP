3ff2d21be8234c45693e8f3449f9da021e32051a
Neil Mayo <Neil.Mayo.c10317@mcast.edu.mt>
2020-11-04 14:15:03 +0100

    Created IProductsService, ProductsService, and made the connection between Application Layer and Data Layer

---
+++ SWD62AEP/ShoppingCart.Application/Interfaces/IProductsService.cs
@@ -0,0 +1,20 @@
+﻿using ShoppingCart.Application.ViewModels;
+using ShoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart.Application.Interfaces
+{
+    public interface IProductsService
+    {
+        //to expose directly your class as it was created originally to create your database is not good practice
+        //hence we create a replica of that class removing nay properties which may disclose some confidential or 
+        //unwanted information
+        //typical eg. if you have user class, would you pass on the 
+        IQueryable<ProductViewModel> GetProducts();
+
+       //void RateProduct(Guid id, string comment, double rating);
+    }
+}
---
+++ SWD62AEP/ShoppingCart.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ShoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Application.ViewModels
+{
+
+    /// <summary>
+    /// This class will be the model that you are going to pass on to whoever is using your classes 
+    /// remember: it could be someone whis is paying for the service 
+    /// //        it could be some 3rd pary company/developer which is not trustworthy
+    /// </summary>
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public double Price { get; set; }
+       
+        public string Description { get; set; }
+        
+        public string ImageUrl { get; set; }
+
+        public CategoryViewModel Category { get; set; }
+
+
+    }
+}
